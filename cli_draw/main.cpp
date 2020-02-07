b4514b8d164478b7c66c4eeaea5d27f788038a56
ctdove <carrot4105@gmail.com>
2020-02-07 16:07:40 +0000

    new input in int main works

--- cli_draw/main.cpp
+++ cli_draw/main.cpp
@@ -1,9 +1,17 @@
 #include "interface.hpp"
+#include <ostream>
 
 int main()
 {
+    int width, height;
+    std::cout << "enter width: ";
+    std::cin >> width;
+    std::cout << std::endl
+              << "enter height: ";
+    std::cin >> height;
+    std::cout << std::endl;
     Clidraw::Grid grid;
 
-    grid.rect(0, 0, 8, 5);
+    grid.rect(0, 0, width, height);
     return 0;
 };
