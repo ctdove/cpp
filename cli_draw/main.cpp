d9b9b419fdcb2bb85e31c3579754bed38353c990
ctdove <carrot4105@gmail.com>
2020-02-07 15:58:13 +0000

    deleted extra function calls in main.cpp to speed up compile

--- cli_draw/main.cpp
+++ cli_draw/main.cpp
@@ -2,11 +2,7 @@
 
 int main () {
     Clidraw::Grid grid;
-    grid.fill("@", 11);
-    std::cout << std::endl;
-    grid.dot("n", 3, 1);
-    grid.fill("-", 20);
-    std::cout << std::endl;
-    grid.rect(0, 0, 5, 5);
+    
+    grid.rect(0, 0, 8, 5);
     return 0;
 };
